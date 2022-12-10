b187af6b1accfa951dcc3a570be9f4ae53acc2ae
UnLegit <ZubMax2005@yandex.ru>
2022-12-10 18:17:01 +0300

    initial commit

---
+++ src/main/java/ru/unlegit/nyc/NewYearCongratulationsApplication.java
@@ -0,0 +1,12 @@
+package ru.unlegit.nyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewYearCongratulationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewYearCongratulationsApplication.class, args);
+    }
+}
